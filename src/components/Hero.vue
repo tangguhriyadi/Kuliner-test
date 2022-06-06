6936c7086defd00b4ce54465c319d1298a5c461b
tangguhriyadi <mtangguh97@gmail.com>
2022-06-06 18:43:50 +0700

    configure responsive display

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,59 @@
 <template>
-  <div class="row mt-2">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2>
-            <strong>Delicious Food Menu, </strong> <br />
-            in Your Gadget
-            <p>Choose your favorite food !</p>
-            <button class="btn btn-lg btn-success"><i class="bi bi-arrow-right"></i> Order Here</button>
-          </h2>
+  <div class="hero">
+    <!-- desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong> <br />
+                in Your Gadget
+                <p>Choose your favorite food !</p>
+                <button class="btn btn-lg btn-success">
+                  <i class="bi bi-arrow-right"></i> Order Here
+                </button>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img
+            class="mainpic"
+            src="../assets/images/main.png"
+            alt="gambar"
+            width="500"
+          />
         </div>
       </div>
     </div>
-    <div class="col">
-      <img src="../assets/images/main.png" alt="gambar" width="500" />
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <img
+            class="mainpic"
+            src="../assets/images/main.png"
+            alt="gambar"
+            width="500"
+          />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong> <br />
+                in Your Gadget
+                <p>Choose your favorite food !</p>
+                <button class="btn btn-lg btn-success">
+                  <i class="bi bi-arrow-right"></i> Order Here
+                </button>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
