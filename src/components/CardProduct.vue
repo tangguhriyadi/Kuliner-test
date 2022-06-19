c330914ffc2e6f1d4902edcf1d7d2d1433f93382
tangguhriyadi <mtangguh97@gmail.com>
2022-06-19 17:28:44 +0700

    fix img size

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card shadow card-product" style="width: 18rem;">
-  <img :src="'assets/images/' + product.gambar " class="card-img-top" alt="...">
+  <img :src="'assets/images/' + product.gambar " class="card-img-top cardimg" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{product.name}}</h5>
     <p class="card-text">Harga: Rp.{{product.price}}</p>  
